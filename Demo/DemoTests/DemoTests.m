47c26ca56f1ff4be261b90ca5fbbcdf6c1542eb0
yidao <yidao@xiaokakeji.com>
2015-07-21 11:13:42 +0800

    commit

--- Demo/DemoTests/DemoTests.m
+++ Demo/DemoTests/DemoTests.m
@@ -23,6 +23,7 @@
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    // nothing
 }
 
 - (void)testExample {
