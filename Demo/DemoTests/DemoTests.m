de3f9287797baa484012796152b0d66eaac06bb5
unko <supperwc@126.com>
2015-07-21 13:18:24 +0800

    commit

--- Demo/DemoTests/DemoTests.m
+++ Demo/DemoTests/DemoTests.m
@@ -23,8 +23,6 @@
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
-//     test SSH KEY
-//    test
 
 }
 
